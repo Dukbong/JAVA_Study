7305158b6fca06bea95620d2c1a174eb953f850a
HyeonSung Jang <gorae930301@gmail.com>
2022-07-21 23:18:31 +0900

    JAVA

---
+++ Data_calculation/dataType.java
@@ -0,0 +1,17 @@
+public class dataType{
+	public static void main (String[] args) {
+		// data type = 숫자, 글자 등등등...
+		System.out.println(6); // Number
+		// 이거 쉽게 치는 방법 sout ctrl+space
+		System.out.println("six"); // String
+		System.out.println("6"); // String
+		
+		System.out.println(6+6); // 12 (Number)
+		System.out.println("6"+"6"); // 66 (String)
+		
+		System.out.println(6*6); // 36 (Number)
+		//System.out.println("6"*"6"); 문자열은 곱할 수 없다.
+		
+		System.out.println("111111".length()); // 6 (Number)
+	}
+}
---
+++ HelloWorld/HelloWorldApp.java
@@ -0,0 +1,7 @@
+public class HelloWorldApp{
+			 //해당 파일명과 동일해야한다.
+			 //	Class 변수명을 기준으로 .class 파일을 실행하기 때문이다.
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
